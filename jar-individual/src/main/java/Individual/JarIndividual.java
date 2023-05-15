f6ca5dd828d52836c2fdfec957abc77cc4e6e253
didiegovieira <diego.vieira@sptech.school>
2023-05-14 22:22:46 -0300

    correção

--- jar-individual/src/main/java/Individual/JarIndividual.java
+++ jar-individual/src/main/java/Individual/JarIndividual.java
@@ -19,6 +19,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class JarIndividual {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
+        Scanner leitor2 = new Scanner(System.in);
         
         ConexaoBancoLocal conexaoBancoLocal = new ConexaoBancoLocal();
         ConexaoBanco conexaoBanco = new ConexaoBanco();
@@ -28,21 +29,23 @@ public class JarIndividual {
         
         SelectUser select = new SelectUser();
         
-        System.out.println("1 SQL / 2 MySql");
-        Integer escolha = leitor.nextInt();
-        String email = "";
-        String senha = "";
+        Integer escolha;
         
-        switch (escolha) {
+        
+        do {
+            System.out.println("1 SQL / 2 MySql");
+            escolha = leitor.nextInt();
+            
+            switch (escolha) {
             case 1:
                 System.out.println("-".repeat(30));
                 System.out.println("Teste Sql pela Azure");
 
                 System.out.println("email");
-                email = leitor.nextLine();
+                String email = leitor2.nextLine();
 
                 System.out.println("senha");
-                senha = leitor.nextLine();
+                String senha = leitor2.nextLine();
 
                 select.selectAndInsert(email, senha);
                 break;
@@ -52,19 +55,21 @@ public class JarIndividual {
                 System.out.println("Teste Sql Local");
 
                 System.out.println("email");
-                email = leitor.nextLine();
+                String emailLocal = leitor.nextLine();
 
                 System.out.println("senha");
-                senha = leitor.nextLine();
+                String senhaLocal = leitor2.nextLine();
 
-                select.selectAndInsertLocal(email, senha);
+                select.selectAndInsertLocal(emailLocal, senhaLocal);
                 break;
                 
             default:
                 System.out.println("Opção inválida. Selecione 1 ou 2.");
                 break;
                 
-        }
+            }   
+        } while (escolha != 0);
+        
         
         
 //        listaObjetoUsuario = conexao.query("select * from Usuario where email = ? and senha = ?", 
